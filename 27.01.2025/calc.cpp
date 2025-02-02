fd97a1c6f9d296c91d236a8e809afa23508fc8dc
milentonoyan <milentonoyann@gmail.com>
2025-02-02 21:30:58 +0400

    Remove embedded homework repository

--- 27.01.2025/calc.cpp
+++ 27.01.2025/calc.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <cmath>
-#include <iostream>
-#include <cmath>
+
 int main() {
     double a, b;
     char c;
-    std::cin>>a;
-    std::cin>>c;
-    std::cin>>b;
-    
-    switch (c){
+
+    std::cin >> a >> c >> b;
+
+    switch (c) {
         case '+':
-            std::cout<<a+b<<'\n';
+            std::cout << a + b << '\n';
             break;
         case '-':
-            std::cout<<a-b<<'\n';
+            std::cout << a - b << '\n';
             break;
         case '/':
-            std::cout<<a/b<<'\n';
+            if (b != 0)
+                std::cout << a / b << '\n';
+            else
+                std::cout << "Թիվը չի բաժանվում 0-ի\n";
             break;
         case '*':
-            std::cout<<a*b<<'\n';
+            std::cout << a * b << '\n';
             break;
+        default:
+            std::cout << "Սխալ\n";
     }
+
     return 0;
 }
